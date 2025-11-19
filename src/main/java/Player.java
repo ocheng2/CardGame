c59a8c69294d02564a14d9fb06aac6576571766f
oliviacheng <olivia.cheng@menloschool.org>
2025-11-18 18:44:05 -0800

    Player Class: added instance variable, constructor and getter methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Player {
+    private String name;
+    private ArrayList<Card> hand;
+    private int points;
+
+    public Player(String name)
+    {
+        this.name = name;
+        points = 0;
+    }
+
+    public Player (String name, ArrayList<Card> hand)
+    {
+        this.name = name;
+        this.hand = hand;
+        points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 }
+
+
