1db19dbc4bb49cb0bf70d6470c4c153793078fc2
oliviacheng <olivia.cheng@menloschool.org>
2025-11-20 10:53:08 -0800

    Finished the basic functionality of the Deck class. Included a isEmpty() method, a getCardsLeft() method, a deal() method, and a shuffle() method.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -6,12 +6,15 @@ public class Deck {
 
     public Deck(String[] ranks, String[] suits, int[] values)
     {
+        cards = new ArrayList<Card>();
+
         for (int i = 0; i < ranks.length; i++)
         {
             cards.add(new Card(ranks[i], suits[i], values[i]));
         }
 
         cardsLeft = cards.size();
+        shuffle();
     }
 
     public boolean isEmpty()
