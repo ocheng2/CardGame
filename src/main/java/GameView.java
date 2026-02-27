390a405fdb588e9933bac75de54f09fa11c16b7d
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-02-26 21:02:53 -0800

    Included constants, draw functions for different aspects of the front end (for example, draw UNO)

--- src/main/java/GameView.java
+++ src/main/java/GameView.java
@@ -7,6 +7,16 @@ public class GameView extends JFrame {
     private final int WINDOW_WIDTH = 1000;
     private final int WINDOW_HEIGHT = 800;
     public final int TITLE_BAR_HEIGHT = 23;
+
+    // Position of the Player cards
+    public final int X_VER = 200;
+    public final int Y_VER_1 = 50;
+    public final int Y_VER_2 = 660;
+
+    public final int Y_HOR = 600;
+    public final int X_HOR_1 = 860;
+    public final int X_HOR_2 = 50;
+
     private Game backend;
 
     private Image gameImage;
@@ -29,44 +39,126 @@ public class GameView extends JFrame {
 
         int state = backend.getState();
 
-        if (state == Game.STATE_INSTRU) {
+        if (state == Game.STATE_BEGIN) {
+          drawUNO(g);
+        }
+        else if (state == Game.STATE_INSTRU) {
             g.drawImage(instructionsImage, 0, 0, this);
         } else if (state == Game.STATE_MAIN) {
             paintGame(g);
+            paintPlayers(g, backend.getPlayers());
+
+            actionButtons(g, "PLACE A CARD", 370, 360);
+            actionButtons(g, "DRAW A CARD", 510, 360);
+
             paintTopCard(g, backend.getTopCard());
-            paintDeck(g, backend.getCurrPlayer());
+            paintPlayers(g, backend.getPlayers());
         } else if (state == Game.STATE_END) {
             g.setColor(Color.green);
         }
     }
 
+    public void drawUNO(Graphics g) {
+        g.setColor(new Color(1,86,75));
+        g.fillRect(0, 0, 1000, 800);
+
+        g.setFont(new Font("Serif", Font.ITALIC, 200));
+        g.setColor(Color.WHITE);
+        g.drawString("UNO", 110, 600);
+
+        g.setFont(new Font("Serif", Font.ITALIC, 30));
+        g.drawString("Press Enter to Begin", 130, 650 );
+    }
+
     public void paintTopCard(Graphics g, Card c) {
-        c.draw(g, 500,400);
+        c.draw(g, 420,430);
+        c.drawBack(g, 520, 430, false);
     }
 
-    public void actionButtons(Graphics g) {
+    public void actionButtons(Graphics g, String text, int x, int y) {
+        int width = 120;
+        int height = 45;
 
+        g.setColor(new Color(19,56,95));
+        g.fillRoundRect(x, y, width, height, 12, 12);
+
+        g.setFont(new Font("Monospaced", Font.PLAIN, 15));
+        g.setColor(Color.WHITE);
+
+        int textX = x + 10;
+        int textY = y + 30;
+        g.drawString(text, textX, textY);
     }
 
-    // Draw the player's hand
-    public void paintDeck (Graphics g, Player p) {
-        ArrayList<Card> hand = p.getHand();
-        String name = p.getName();
+    public void paintPlayers(Graphics g, Player[] players) {
+        int currX = 100;
+        int currY = 100;
+        int x, y;
 
-        // Coordinates of the card
-        int x = 100;
-        int y = 700;
+        for (int i = 0; i < players.length; i++) {
+            // Set up the player
+            Player p = players[i];
+            int positionIndex = (i - backend.getCurrPlayerIndex() + players.length) % players.length;;
 
-        // Iterates through player's hand to draw each one
-        for (Card c: hand) {
-            c.draw(g, x, y);
+            // Set up the font
+            g.setFont(new Font("Serif", Font.ITALIC, 15));
+            g.setColor(Color.WHITE);
 
-            // Ensures the cards do not overlap
-            x += 70;
+            if (positionIndex == 0) {
+                x = X_VER;
+                y = Y_VER_2;
+            } else if (positionIndex == 1) {
+                x = X_HOR_2;
+                y = Y_HOR;
+            } else if (positionIndex == 2) {
+                x = X_VER;
+                y = Y_VER_1;
+            } else {
+                x = X_HOR_1;
+                y = Y_HOR;
+            }
+
+            drawName(g, p, x, y, positionIndex);
+
+            ArrayList<Card> hand = p.getHand();
+
+
+            // Iterates through player's hand to draw each one
+            for (int j = 0; j < hand.size(); j++) {
+                Card c = hand.get(j);
+
+                if (positionIndex == 0) {
+                    c.draw(g, x, y);
+
+                    // Ensures the cards do not overlap
+                    x += 70;
+                } else if (positionIndex % 2 == 1){
+                    c.drawBack(g, x, y, true);
+
+                    // Ensures the cards do not overlap
+                    y -= 70;
+                } else {
+                    c.drawBack(g, x, y, false);
+
+                    // Ensures the cards do not overlap
+                    x += 70;
+                }
+            }
         }
     }
 
+    public void drawName(Graphics g, Player p, int x, int y, int positionIndex) {
+        if (positionIndex % 2 == 0) {
+            y += 110;
+        } else {
+            y += 80;
+        }
+
+        g.drawString("Player: " + p.getName(), x, y);
+    }
+
     public void paintGame(Graphics g) {
-        g.drawImage(gameImage, 0, 0, this);
+        g.setColor(new Color(1,86,75));
+        g.fillRect(0, 0, 1000, 800);
     }
 }
